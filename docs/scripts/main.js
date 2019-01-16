20a0085edeec25c6ef0b475ae472bcc1cce52831
MarVel95 <silence95@mail.bg>
2019-01-16 11:07:10 +0200

    Code optimized to work with vanilla JS, jQuery removed

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,4 +1,5 @@
 document.getElementById('menuIcon').addEventListener('click', toggleMenu);
+
 let submenus = [].slice.call(document.getElementsByClassName('submenu'));
 submenus.forEach(submenu => submenu.addEventListener('click', toggleSubmenu));
 
@@ -9,11 +10,14 @@ function toggleMenu() {
         if (this.classList.contains('isActive')) {
             this.classList.remove('isActive');
             menu.classList.remove('isActive');
-            document.getElementById('menuContainer').removeChild(document.getElementById('overlay'));
+
+            document.getElementById('menuContainer')
+                    .removeChild(document.getElementById('overlay'));
         }
         else {
             this.classList.add('isActive');
             menu.classList.add('isActive');
+
             let overlay = document.createElement("div");
             overlay.id = "overlay";
             menuContainer.insertAdjacentElement('afterbegin', overlay);
@@ -23,18 +27,22 @@ function toggleMenu() {
 
 function toggleSubmenu() {
     if(window.matchMedia("(max-width: 1100px)").matches) {
-        let submenu = $(this);
-        let submenuChild = $(submenu).children().eq(1);
-        if (submenu.hasClass('isActive')) {
-            submenu.removeClass('isActive');
-            submenuChild.css('height', "");
+        let submenuParent = this;
+        let submenu = submenuParent.children[1];
+        if (submenuParent.classList.contains('isActive')) {
+            submenuParent.classList.remove('isActive');
+            submenu.setAttribute('style', '');
         }
         else {
-            submenu.addClass('isActive');
-            submenuChild.addClass('show');
-            let offset = submenuChild.height();
-            submenuChild.removeClass('show');
-            submenuChild.css('height', offset + 'px');
+            submenuParent.classList.add('isActive');
+
+            submenu.style.height = "auto";
+            let offset = submenu.offsetHeight;
+
+            submenu.style.height =  `0px`;
+            setTimeout(() => {
+                submenu.style.height = offset + `px`;
+            }, 1);
         }
     }
 }
