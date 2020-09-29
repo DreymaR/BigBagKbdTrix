2cde898b55ba4d7ddd89cf4b194c2667aff61b6d
DreymaR <obgadmar@gmail.com>
2020-09-29 16:40:08 +0200

    Favicon
    * Added a favicon (ico64_b6) by link
    * Reorganized fonts, script, css
    * Added resource files from Dropbox

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# BigBagKbdTricks
-DreymaR's Big Bag of Keyboard Tricks
+# BigBagKbdTricks docs
+## DreymaR's Big Bag of Keyboard Tricks - The Web Pages
+=======================================================
+
+TODO/WIP for DreymaR's Big Bag Of Keyboard Tricks content (Forum & html):
+-------------------------------------------------------------------------
+- Dictionary: Explain SFB, Curl/Angle/Wide, inward/outward/mixed rolls
+- Make Image2 and Image3 posts for the Image topic (topic 111)
+	- Image2: Keyboard images. State licencing for SVG (CC) vs others (Public Domain)
+	- Image3: Interesting designs, fun, logos, icons, etc
+
+TODO/WIP For the BigBag web pages:
+----------------------------------
+- Include the header code on all pages instead of duplicating code! May have to use JS. Or jQuery.load()?
+	- See https://www.w3schools.com/howto/howto_html_include.asp
+	- Or https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file (jQuery)
+- Make a Co browser tab (fav)icon
+	- See https://stackoverflow.com/questions/4888377/how-to-add-a-browser-tab-icon-favicon-for-a-website
+- Make one img (or res?) folder from the Dropbox Public one, and one html or www/web
+	- Put css and js in a separate script or res folder, or in www?
+
+
+HTML tips from MarVel95, 2019-09-17:
+------------------------------------
+```
+• https://developer.mozilla.org/ - documentation for everything that has to do with HTML and CSS
+
+• tag=element – the most used tags are:
+<p> - paragraph
+    <p class="italic">
+<a> - link
+    <a class="underlined">
+<div class="quote">
+<b>, <strong> - bold
+<em> - italic
+<img src="content/images/dk12_dblacutesci.png" class="smallImg"> (alt attribute is optional - displays when image cannot be displayed)
+    <img class="left"....
+ul - unordered list
+ol - ordered list (1,2,3/a,b,c)
+    <ul class="disc">
+    <ul class="circle">
+
+<span> - separates a text so you can style it, for example if you want part of a sentence to be crossed with a line, go
+    <span class="linethrough">
+
+• For spoilers:
+<div class="spoiler">
+    <p><span>Spoiler:</span></p>
+    <div class="spoilerBody">
+    SPOILER CONTENT HERE
+    </div>
+</div>
+
+• To display the characters '<' and '>', use &lt; and &gt; so that they're not interpreted as parts of a tag
+
+• For blocks of code use <pre><code>...</code><pre> and remove all indentation(tabs) left of the code in between these tags 
+    because the tabs will be shown as these tags format literally every space, tab and new line.
+```
---
+++ docs/web/main.js
@@ -0,0 +1,226 @@
+document.getElementById('menuIcon').addEventListener('click', toggleMenu);
+
+let submenus = [].slice.call(document.getElementsByClassName('submenu'));
+submenus.forEach(submenu => submenu.addEventListener('click', toggleSubmenu));
+
+let spoilers = [].slice.call(document.getElementsByClassName('spoiler'));
+spoilers.forEach(spoiler => spoiler.children[0].addEventListener('click', toggleSpoiler));
+
+let platformsEl = document.getElementById('platformIcons').children;
+let platforms = {
+    'linux': platformsEl[0],
+    'tmk': platformsEl[1],
+    'win': platformsEl[3]
+}
+
+let platformWisePages = ['ergomods', 'keymappings', 'otherlayouts'];
+
+document.onload = function () {
+    function () {
+        includeHTML();
+    }
+    
+    let platform = sessionStorage.getItem('platform');
+    if (platform) {
+        platforms[platform].classList.add('isActive');
+        page = getCurrentPageName();
+        if (!(page.includes(platform))) {
+            checkForPlatformPage(platform);
+        }
+        document.getElementById('platforms').classList.add(platform);
+    }
+    else {
+        document.getElementById('platforms').removeAttribute('class');
+    }
+    
+
+    Object.keys(platforms).forEach(platform => function (){
+        platforms[platform].addEventListener('click',
+            function () {
+                togglePlatform(platform);
+            }
+        );
+    }());
+}();
+
+function getCurrentPageName() {
+    let path = window.location.pathname;
+    let page = path.split("/").pop();
+    return page;
+}
+
+function redirect(destination) {
+    if(destination!='ergomods-win.html'){
+        window.location.href = destination;
+    }
+}
+
+function checkForPlatformPage(platform){
+    platformWisePages.forEach(function (platformWisePage) {
+        if (getCurrentPageName().includes(platformWisePage)) {
+            redirect(platformWisePage + '-' + platform + '.html')
+        }
+    });
+}
+
+function togglePlatform(platform) {
+    let currentPlatform = sessionStorage.getItem('platform');
+    page = getCurrentPageName();
+    
+    if (currentPlatform == platform) {
+        sessionStorage.removeItem('platform');
+        platforms[platform].classList.remove('isActive');
+        if (getCurrentPageName().includes(platform)) {
+            redirect(page.substring(0, page.indexOf('-')) + '.html');
+        }
+        document.getElementById('platforms').removeAttribute('class');
+    }
+    else {
+        if (currentPlatform) {
+            Object.keys(platforms).forEach(function (platform) {
+                console.log();
+                if (platforms[platform].classList.contains('isActive')) {
+                    platforms[platform].classList.remove('isActive');
+                }
+            });
+        }
+        document.getElementById('platforms').removeAttribute('class');        
+        document.getElementById('platforms').classList.add(platform);
+        sessionStorage.setItem('platform', platform);
+        checkForPlatformPage(platform);
+        platforms[platform].classList.add('isActive');
+    }
+}
+
+function toggleMenu() {
+    if (window.matchMedia("(max-width: 1100px)").matches) {
+        let menu = document.getElementById('menu');
+        let menuContainer = document.getElementById('menuContainer');
+        if (this.classList.contains('isActive')) {
+            this.classList.remove('isActive');
+            menu.classList.remove('isActive');
+
+            document.getElementById('menuContainer')
+                .removeChild(document.getElementById('overlay'));
+        }
+        else {
+            this.classList.add('isActive');
+            menu.classList.add('isActive');
+
+            let overlay = document.createElement("div");
+            overlay.id = "overlay";
+            menuContainer.insertAdjacentElement('afterbegin', overlay);
+        }
+    }
+}
+
+function toggleSubmenu(e) {
+    if (window.matchMedia("(max-width: 1100px)").matches) {
+        let submenuParent = this;
+        let submenu = submenuParent.children[1];
+        if (submenuParent.classList.contains('isActive')) {
+            submenuParent.classList.remove('isActive');
+            submenu.setAttribute('style', '');
+        }
+        else {
+            e.preventDefault();
+            submenuParent.classList.add('isActive');
+
+            submenu.style.height = "auto";
+            let offset = submenu.offsetHeight;
+
+            submenu.style.height = `0px`;
+            setTimeout(() => {
+                submenu.style.height = offset + `px`;
+            }, 1);
+        }
+    }
+}
+
+function toggleSpoiler() {
+    let spoilerBody = this.parentNode.getElementsByClassName("spoilerBody")[0];
+    if (spoilerBody.classList.contains('isActive')) {
+        spoilerBody.classList.remove('isActive');
+        spoilerBody.parentNode.classList.remove('isActive');
+    }
+    else {
+        spoilerBody.classList.add('isActive');
+        spoilerBody.parentNode.classList.add('isActive');
+    }
+}
+
+if (window.matchMedia("(max-width: 1100px)").matches) {
+    if (!localStorage.getItem('website_visited')) {
+        drawTutorialScreen();
+    }
+}
+
+function drawTutorialScreen() {
+    let tutorialScreen = document.createElement('div');
+    tutorialScreen.id = 'tutorialScreen';
+
+    let platformIcon = document.createElement('div');
+    platformIcon.id = 'tutorialPlatform';
+
+    let arrow = document.createElement('img');
+    arrow.setAttribute('src', 'w_img/arrow.png');
+    arrow.id = 'tutorialArrow';
+
+    let text = document.createElement('span');
+    text.innerText = 'Click here to toggle the desired platform.';
+    text.id = 'tutorialText';
+
+    let closeDiv = document.createElement('a');
+    closeDiv.id = 'tutorialClose';
+
+    let closeText = document.createElement('span');
+    closeText.id = 'tutorialCloseText';
+    closeText.innerText = 'Got it';
+
+    let closeButton = document.createElement('div');
+    closeButton.id = 'tutorialCloseButton';
+
+    closeDiv.append(closeText);
+    closeDiv.append(closeButton);
+    closeDiv.addEventListener('click', removeTutorialScreen);
+
+    tutorialScreen.append(platformIcon);
+    tutorialScreen.append(arrow);
+    tutorialScreen.append(text);
+    tutorialScreen.append(closeDiv);
+
+    document.body.append(tutorialScreen);
+    localStorage.setItem('website_visited', true);
+}
+
+function removeTutorialScreen() {
+    document.body.removeChild(tutorialScreen);
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
